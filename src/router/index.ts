220f3a4b88e32b80cd41512d45794d39e9ba036a
cklwblove <lwbhtml@163.com>
2019-01-23 17:57:58 +0800

    refactor: 优化

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,14 +5,14 @@
  * @description 定义路由模块
  */
 
-import Vue from 'vue';
-import Router from 'vue-router';
+import Vue, {AsyncComponent} from 'vue';
+import Router, {RouteConfig} from 'vue-router';
 
 Vue.use(Router);
 
-const loadView = (view: string) => () => import(`@views/${view}/index.vue`);
+const loadView = (view: string): AsyncComponent => (): any => import(`@views/${view}/index.vue`);
 
-const routes = [
+const routes: RouteConfig[] = [
   {
     path: '/',
     name: 'hello',
@@ -29,6 +29,6 @@ const routes = [
 ];
 
 export default new Router({
-    mode: 'hash',
-    routes
+  mode: 'hash',
+  routes
 });
