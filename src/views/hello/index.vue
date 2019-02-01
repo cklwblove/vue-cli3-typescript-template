72696635c7dde82259a6c83844e8a1ba5df339b6
cklwblove <lwbhtml@163.com>
2019-02-01 10:13:06 +0800

    fix: 修复 bug

--- src/views/hello/index.vue
+++ src/views/hello/index.vue
@@ -1,72 +1,73 @@
-class as div as template; = 'page page-hello' >
-    class as; div = 'page-content' >
-      -- 静态资源路径写法事例 as !; -- >
-      src as img; = '~@assets/img/logo.png' >
-      v as h1 - text; = 'msg' > /h1> as 
-      < h2; v - text; = 'message' > /h2> as 
-      < svg - icon; icon - class; = 'cat' > /svg-icon> as 
-      < send - code; class= 'btn btn-default'; v - model; = 'start'; @click.native= 'sendCode' > /send-code> as 
-      < div; class= 'demo' >
-        方法示例 as h3< / h3 >
+<template>
+  <div class="page page-hello">
+    <div class="page-content">
+      <!-- 静态资源路径写法事例 -->
+      <img src="~@assets/img/logo.png">
+      <h1 v-text="msg"></h1>
+      <h2 v-text="message"></h2>
+      <svg-icon icon-class="cat"></svg-icon>
+      <send-code class="btn btn-default" v-model="start" @click.native="sendCode"></send-code>
+      <div class="demo">
+        <h3>方法示例</h3>
         <pre>
-          & lt; template & gt; 
-& lt; div; class= & quot; page; page - hello & quot; & gt; 
-& lt; !-- 静态资源路径写法事例; -- & gt; 
-& lt; img; src = & quot; ~; @assets/images/copyfiles / logo.png & quot; & gt; 
-& lt; p; v - text; = & quot; msg & quot; & gt; & lt; /p&gt;
-            & lt; /div&gt;
-          & lt; /template&gt;
+          &lt;template&gt;
+            &lt;div class=&quot;page page-hello&quot;&gt;
+              &lt;!-- 静态资源路径写法事例 --&gt;
+              &lt;img src=&quot;~@assets/images/copyfiles/logo.png&quot;&gt;
+              &lt;p v-text=&quot;msg&quot;&gt;&lt;/p&gt;
+            &lt;/div&gt;
+          &lt;/template&gt;
 
-          & lt; script; lang = & quot; ts & quot; & gt;
+          &lt;script lang=&quot;ts&quot;&gt;
             /**
              * 以下仅为事例代码，可以随意扩展修改
              */
-import {Component, Vue} from  & ; #x27; vue - property - decorator & ; #x27;
+            import {Component, Vue} from &#x27;vue-property-decorator&#x27;;
             // 工具类
-import {formatDate} from  & ; #x27; utils & ; #x27;
+            import {formatDate} from &#x27;utils&#x27;;
 
-@Component
+            @Component
             export default class Hello extends Vue {
-public x27; public Welcome; public to; public Your; public Vue; .public js;               private msg: string =  & ; #+ TypeScript; App & ; #x27;
-              private message: string =  & ; #x60; 现在时间是; ：$; {formatDate(Date.now()); } & ; #x60;
+              private msg: string = &#x27;Welcome to Your Vue.js + TypeScript App&#x27;;
+              private message: string = &#x60;现在时间是：${formatDate(Date.now())}&#x60;;
 
-              private created(); {
+              private created() {
                 this.movieComingSoon();
               }
 
-              private movieComingSoon(); {
+              private movieComingSoon() {
                 const data = {};
                 this.$services.octocat({
-                  method:  & #x27; get & #x27; ,
-                data;
-                }).then((res) = & gt; {
-                  console.log( & #x27; 接口请求成功; ： & ; #x27; + JSON.stringify(res, null, 2); )
-                }). catch ((err) = & gt; {
-                  console.log( & #x27; 接口请求异常; ： & ; #x27; + err; )
-                })
+                  method: &#x27;get&#x27;,
+                  data
+                }).then((res) =&gt; {
+                  console.log(&#x27;接口请求成功：&#x27; + JSON.stringify(res, null, 2));
+                }).catch((err) =&gt; {
+                  console.log(&#x27;接口请求异常：&#x27; + err);
+                });
               }
 
             }
-& lt; /script&gt;
+          &lt;/script&gt;
 
-          & lt; style; lang = & quot; less & quot; rel = & quot; stylesheet / less & quot; & gt;
-@import  & quot; ./style.less&quot;;
-          & lt; /style&gt;
-        < /pre>
-      < /div>
-    < /div>
-  < /div>
-< /template>
+          &lt;style lang=&quot;less&quot; rel=&quot;stylesheet/less&quot;&gt;
+            @import &quot;./style.less&quot;;
+          &lt;/style&gt;
+        </pre>
+      </div>
+    </div>
+  </div>
+</template>
 
-< script; lang = 'ts' > ;
+<script lang="ts">
   /**
    * 以下仅为事例代码，可以随意扩展修改
    */
-import {Component, Vue} from 'vue-property-decorator';
+  import {Component, Vue} from 'vue-property-decorator';
   // 工具类
-import {formatDate} from 'utils';
+  import {formatDate} from 'utils';
 
-@Component
+  @Component
   export default class Hello extends Vue {
     private msg: string = 'Welcome to Your Vue.js + TypeScript App';
     private message: string = `现在时间是：${formatDate(Date.now())}`;
@@ -87,8 +88,8 @@ import {formatDate} from 'utils';
       });
     }
   }
-/script> as 
+</script>
 
-< style; lang = 'less'; rel = 'stylesheet/less' > ;
-@import './style.less';
-/style> as ;
+<style lang="less" rel="stylesheet/less">
+  @import "./style.less";
+</style>
