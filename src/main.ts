a05778d876039b94e2bf3fdfb965e671ca7906d0
cklwblove <lwbhtml@163.com>
2019-03-08 10:42:44 +0800

    refactor: 优化代码

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import 'lib-flexible';
 import Vue from 'vue';
-import SvgIcon from 'vue-svgicon';
 import App from './App.vue';
 import router from './router';
-import './icons/components';
+import './icons';
 import './router/router.interceptor';
 import './filters';
 import './services';
 import './registerServiceWorker';
-
-Vue.use(SvgIcon, {
-  tagName: 'svg-icon',
-  defaultWidth: '1em',
-  defaultHeight: '1em',
-});
 
 Vue.config.productionTip = process.env.NODE_ENV === 'production';
 
